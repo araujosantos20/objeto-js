22c82d15d67d8ccef350fbfb3ad52253821d0985
Mateus Santos <mateus.sanjo20@gmail.com>
2024-09-28 01:39:54 -0300

    Criando uma calculadora com objetos e arrays, criando os métodos de soma, subtração, multiplicação, divisão e calcular média

--- info-pessoa.js
+++ info-pessoa.js
@@ -26,29 +26,58 @@
 
 // console.log(mostrarInfoPessoa(pessoa));
 
-const pessoas = [
-  { nome: "Mateus", idade: 20, cidade: "Santo André" },
-  { nome: "Murilo", idade: 19, cidade: "São Paulo" },
-  { nome: "João", idade: 18, cidade: "São Paulo" },
-];
+// const pessoas = [
+//   { nome: "Mateus", idade: 20, cidade: "Santo André" },
+//   { nome: "Murilo", idade: 19, cidade: "São Paulo" },
+//   { nome: "João", idade: 18, cidade: "São Paulo" },
+// ];
 
-function mostrarListaPessoas(pessoas) {
-  console.log("Lista de Pessoas: ");
-  pessoas.forEach(pessoa => {
-    console.log(`Nome: ${pessoa.nome}, Idade: ${pessoa.idade} anos, Cidade: ${pessoa.cidade}`);
-  });
+// function mostrarListaPessoas(pessoas) {
+//   console.log("Lista de Pessoas: ");
+//   pessoas.forEach(pessoa => {
+//     console.log(`Nome: ${pessoa.nome}, Idade: ${pessoa.idade} anos, Cidade: ${pessoa.cidade}`);
+//   });
+// };
+
+// mostrarListaPessoas(pessoas);
+
+// pessoas.push({nome: "Tiago", idade: 27, cidade: "Ribeirão Preto"});
+// pessoas.push({nome: "Thales", idade: 23, cidade: "São Paulo"});
+
+// mostrarListaPessoas(pessoas);
+
+// function filtrarPorCidade(pessoas, cidade) {
+//   return pessoas.filter(pessoa => pessoa.cidade === cidade);
+// };
+
+// console.log("Pessoas de São Paulo:");
+// console.log(filtrarPorCidade(pessoas, "São Paulo"));
+
+
+const calculadora = {
+  soma: function(n1, n2) {
+    return n1 + n2;
+  },
+  subtracao: function(n1, n2) {
+    return n1 - n2;
+  },
+  multiplicacao: function(n1, n2) {
+    return n1 * n2;
+  },
+  divisao: function(n1, n2) {
+    return n1 / n2;
+  },
+  calcularMedia: function(lista) {
+    const soma = lista.reduce((total, numero) => total + numero, 0);
+    return soma / lista.length;
+  },
 };
 
-mostrarListaPessoas(pessoas);
+console.log("Resultados das operações é:");
+console.log(`Soma: ${calculadora.soma(3, 3)}`);
+console.log(`Subtração: ${calculadora.subtracao(3, 3)}`);
+console.log(`Multiplicação: ${calculadora.multiplicacao(3, 3)}`);
+console.log(`Divisão: ${calculadora.divisao(3, 3)}`);
 
-pessoas.push({nome: "Tiago", idade: 27, cidade: "Ribeirão Preto"});
-pessoas.push({nome: "Thales", idade: 23, cidade: "São Paulo"});
-
-mostrarListaPessoas(pessoas);
-
-function filtrarPorCidade(pessoas, cidade) {
-  return pessoas.filter(pessoa => pessoa.cidade === cidade);
-};
-
-console.log("Pessoas de São Paulo:");
-console.log(filtrarPorCidade(pessoas, "São Paulo"));
+const lista = [10, 4, 30, 15, 6, 7];
+console.log(calculadora.calcularMedia(lista));
