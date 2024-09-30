317c6f1ba7ae6184fa815660606b7a835c19f3e1
Mateus Santos <mateus.sanjo20@gmail.com>
2024-09-30 11:58:51 -0300

    Praticando for in para retornar chaves e valores de um objeto

--- exercicio-objeto.js
+++ exercicio-objeto.js
@@ -1,25 +1,46 @@
-const pessoa = {
-  nome: "Mateus",
-  notas: [10, 8, 8, 6],
-  calcularMediaNotas: function (notas) {
-    const calculo = notas.reduce((nota, soma) => nota + soma, 0);
-    return calculo / notas.length;
-  },
-  classificarDesempenho: function (calcularMediaNotas) {
-    if (calcularMediaNotas >= 9) {
-      return "Desempenho excelente";
-    } else if (calcularMediaNotas >= 7.6) {
-      return "Bom desempenho";
-    } else if (calcularMediaNotas >= 6.1) {
-      return "Desempenho regular";
-    } else {
-      return "Desempenho insuficiente";
-    }
-  },
+// const pessoa = {
+//   nome: "Mateus",
+//   notas: [10, 8, 8, 6],
+//   //   Nesta função vamos utilizar o método reduce, onde ele irá
+//   // pegar cada item da chave notas e somando cada valor dessa lista
+//   // e no final retornar o valor da lista somada divido pelo tamanho da lista.
+//   calcularMediaNotas: function (notas) {
+//     const calculo = notas.reduce((nota, soma) => nota + soma, 0);
+//     return calculo / notas.length;
+//   },
+//   //   Aqui apenas vamos pegar o valor que irá retornar da média que será
+//   // calculada do método acima e conforme o valor, vamos retornar um breve
+//   // texto dizendo como está seu desempenho
+//   classificarDesempenho: function (calcularMediaNotas) {
+//     if (calcularMediaNotas >= 9) {
+//       return "Desempenho excelente";
+//     } else if (calcularMediaNotas >= 7.6) {
+//       return "Bom desempenho";
+//     } else if (calcularMediaNotas >= 6.1) {
+//       return "Desempenho regular";
+//     } else {
+//       return "Desempenho insuficiente";
+//     }
+//   },
+// };
+
+// const calculoMedia = pessoa.calcularMediaNotas(pessoa.notas);
+// const desempenho = pessoa.classificarDesempenho(calculoMedia);
+
+// console.log(calculoMedia);
+// console.log(desempenho);
+
+const carro = {
+  marca: "Ford",
+  modelo: "Mustang GT",
+  ano: 2022,
+  cor: "Vermelho",
+  motor: "V8 5.0L",
+  tetoSolar: true,
 };
 
-const calculoMedia = pessoa.calcularMediaNotas(pessoa.notas);
-const desempenho = pessoa.classificarDesempenho(calculoMedia);
-
-console.log(calculoMedia);
-console.log(desempenho);
+for (car in carro) {
+  let chave = car;
+  let valor = carro[car];
+  console.log(`${chave}: ${valor}`);
+}
