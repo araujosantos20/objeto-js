ec6d84944eefa1164251b12df93a6f6888b55a80
Mateus Santos <mateus.sanjo20@gmail.com>
2024-10-06 17:09:51 -0300

    Criando uma função para criar um cópia de um objeto em JSON

--- projetoJSON/manipulacaoJson.js
+++ projetoJSON/manipulacaoJson.js
@@ -14,3 +14,22 @@ objetoAnimais.animais.splice(1, 1);
 
 const stringAnimais = JSON.stringify(objetoAnimais);
 console.log(stringAnimais);
+
+function copiaObjetoJson(objeto) {
+  return JSON.parse(JSON.stringify(objeto));
+}
+
+const pessoa = {
+  id: 1,
+  nome: "Mateus",
+  idade: 20,
+};
+
+const pessoaModificada = copiaObjetoJson(pessoa);
+
+pessoaModificada.idade = 19;
+
+console.log("Pessoa Original:");
+console.log(pessoa);
+console.log("Pessoa Modificada:");
+console.log(pessoaModificada);
